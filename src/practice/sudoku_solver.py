b125c5c3c751085a79a34bd14bb465d758e29b84
Bryson <bryson.meiling@aggiemail.usu.edu>
2019-12-29 23:31:48 -0700

    sudoku solver works. Time yo work on gui

--- src/practice/sudoku_solver.py
+++ src/practice/sudoku_solver.py
@@ -15,7 +15,7 @@ class Sudoku:
 		self.board = []
 		self.create_board()
 		self.solution = self.board.copy()
-		self.solve()
+		# self.solve()
 
 	def create_board(self):
 		for line in self.board_str.split("\n"):
@@ -31,26 +31,53 @@ class Sudoku:
 
 	def solve(self):
 		# backtracking algorithm
-		pass
 
-	# works for prechecking a move. Makes sure a certain number doesn't already exist in a blocking space
+		lst = [0, 0]
+		if not self.find_next_empty(lst):
+			return True
+
+		for num in range(1, self.size + 1):
+			if self.is_valid(num, lst[0], lst[1]):
+
+				self.solution[lst[0]][lst[1]] = num
+
+				# return if success
+				if self.solve():
+					return True
+
+				# failure, reset
+				self.solution[lst[0]][lst[1]] = 0
+
+		return False
+
+	def find_next_empty(self, lst):
+		row, col = self.solution.shape
+		for i in range(row):
+			for j in range(col):
+				if self.solution[i][j] == 0:
+					lst[0] = i
+					lst[1] = j
+					return True
+
+		return False
+
+	# works for pre-checking a move. Makes sure a certain number doesn't already exist in a blocking space
 	def is_valid(self, value, r_idx, c_idx):
 
-		if value in self.board[:, c_idx] or value in self.board[r_idx]:
+		if value in self.solution[:, c_idx] or value in self.solution[r_idx]:
 			return False
-
 
 		# refers to which third, or part of the board the value is in. If the board is not
 		# the traditional 3x3, then it may be of another size
-		v_part = r_idx//self.chunk_size
-		h_part = c_idx//self.chunk_size
+		v_part = r_idx // self.chunk_size
+		h_part = c_idx // self.chunk_size
 
-		start_row = v_part*self.chunk_size
+		start_row = v_part * self.chunk_size
 		end_row = start_row + self.chunk_size
 		start_col = h_part * self.chunk_size
-		end_col =start_col + self.chunk_size
+		end_col = start_col + self.chunk_size
 
-		if value in self.board[start_row:end_row, start_col:end_col]:
+		if value in self.solution[start_row:end_row, start_col:end_col]:
 			return False
 
 		else:
@@ -82,4 +109,9 @@ except ValueError as e:
 	print("uh oh")
 	print(e)
 
-print(puzzles)
+
+for i in range(5):
+	puzzles[i].solve()
+	print(puzzles[i].solution)
+	print()
+
