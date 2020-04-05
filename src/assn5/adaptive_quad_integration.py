84ecfdf64ae9535a9cd069a853e63938a34af160
Bryson <bryson.meiling@aggiemail.usu.edu>
2020-04-05 15:09:25 -0600

    pdf file with graphs added

--- src/assn5/adaptive_quad_integration.py
+++ src/assn5/adaptive_quad_integration.py
@@ -4,7 +4,11 @@ def adap_quad_integration(a, b, function, tol):
     I_1 = ((b - a) / 6) * (function(a) + 4 * function(c) + function(b))
     I_2 = ((b - a) / 12) * (function(a) + 4 * function((a + c) / 2) + 2 *
                             function(c) + 4 * function((b + c) / 2) + function(b))
+
+    # if the difference between I_1 and I_2 is less that user defined tolerance, return
     if abs(I_1 - I_2) <= tol:
         return 16 * I_2 / 15 - I_1 / 15
     else:
-        return adap_quad_integration(a, c, function, tol) + adap_quad_integration(c, b, function, tol)
+        # else split the area is half and add the sums through recursion stack
+        return adap_quad_integration(a, c, function, tol) +\
+               adap_quad_integration(c, b, function, tol)
