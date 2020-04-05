21cc6b6699e7193f1c69d2e98525634d84d26fb7
Bryson <bryson.meiling@aggiemail.usu.edu>
2020-04-05 13:58:47 -0600

    everything seems to be working. Working on final problem

--- src/assn5/adaptive_quad_integration.py
+++ src/assn5/adaptive_quad_integration.py
@@ -1,5 +1,10 @@
-import numpy as np
-
 # Adaptive Quadrature Integration
-def adap_quad_integration():
-    pass
+def adap_quad_integration(a, b, function, tol):
+    c = (a + b) / 2
+    I_1 = ((b - a) / 6) * (function(a) + 4 * function(c) + function(b))
+    I_2 = ((b - a) / 12) * (function(a) + 4 * function((a + c) / 2) + 2 *
+                            function(c) + 4 * function((b + c) / 2) + function(b))
+    if abs(I_1 - I_2) <= tol:
+        return 16 * I_2 / 15 - I_1 / 15
+    else:
+        return adap_quad_integration(a, c, function, tol) + adap_quad_integration(c, b, function, tol)
