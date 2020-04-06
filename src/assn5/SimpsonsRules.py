b6e8f45b08314bab784b747a1900b77e18cbbb95
Bryson <bryson.meiling@aggiemail.usu.edu>
2020-04-05 18:50:04 -0600

    reduing duplicated code

--- src/assn5/SimpsonsRules.py
+++ src/assn5/SimpsonsRules.py
@@ -1,17 +1,9 @@
 import numpy as np
+from src.assn5.trapazoidal_integration import trap_integration
 
 # the function that is being integrated
 def func(x):
     return 3 * np.power(x, 2) + 5*x - 7
-
-# the trapazoidal integration for n == 1
-def trap_integration(a, b, n, function):
-    # creates the step size
-    step = (b - a) / n
-    # adds the sum of the evaluation of the function at each step from a to b
-    iter_sum = function(a) + function(b) + sum(list(map(lambda x: 2 * function(x), np.arange(a + step, b, step))))
-    # alters the sum by the range of the bounds and 2n
-    return (b - a) * iter_sum / (2 * n)
 
 # simpson's 1/3 integration rule for even amounts of subsets
 def simpson_third_integration(a, b, n, function):
